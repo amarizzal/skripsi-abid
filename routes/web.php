90947a472ceb75a5005216cdd2e8f95460b89347
rizal <rizal.palms@gmail.com>
2025-05-06 22:34:44 +0700

    add dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/schedule', function () {
+    return view('schedule');
 });
