ac520945086d483eae547147f7b7c2df30cd74f3
rizal <rizal.palms@gmail.com>
2025-05-09 09:00:49 +0700

    improve layout

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
-});
+})->name('login');
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->name('dashboard');
 Route::get('/schedule', function () {
     return view('schedule');
-});
+})->name('schedule');
