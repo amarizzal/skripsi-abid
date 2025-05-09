c45a0ba9815e24fddb5deb66f592dffb25f9a999
rizal <rizal.palms@gmail.com>
2025-05-09 08:55:01 +0700

    update tampilan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -253,127 +253,128 @@
                   <tr>
                     <td class="w-30">
                       <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
+                        {{-- <div>
                           <img src="../assets/img/icons/flags/US.png" alt="Country flag">
-                        </div>
+                        </div> --}}
                         <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">Country:</p>
-                          <h6 class="text-sm mb-0">United States</h6>
+                          <p class="text-xs font-weight-bold mb-0">Event:</p>
+                          <h6 class="text-sm mb-0">Rapat Bulanan</h6>
                         </div>
                       </div>
                     </td>
                     <td>
                       <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                        <h6 class="text-sm mb-0">2500</h6>
+                        <p class="text-xs font-weight-bold mb-0">Time:</p>
+                        <h6 class="text-sm mb-0">17:00</h6>
                       </div>
                     </td>
                     <td>
                       <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Value:</p>
-                        <h6 class="text-sm mb-0">$230,900</h6>
+                        <p class="text-xs font-weight-bold mb-0">Place:</p>
+                        <h6 class="text-sm mb-0">Balaikota</h6>
                       </div>
                     </td>
                     <td class="align-middle text-sm">
                       <div class="col text-center">
-                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                        <h6 class="text-sm mb-0">29.9%</h6>
+                        <p class="text-xs font-weight-bold mb-0">Status:</p>
+                        <h6 class="text-sm mb-0">Internal</h6>
                       </div>
                     </td>
                   </tr>
                   <tr>
                     <td class="w-30">
                       <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
-                          <img src="../assets/img/icons/flags/DE.png" alt="Country flag">
-                        </div>
+                        {{-- <div>
+                          <img src="../assets/img/icons/flags/US.png" alt="Country flag">
+                        </div> --}}
                         <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">Country:</p>
-                          <h6 class="text-sm mb-0">Germany</h6>
+                          <p class="text-xs font-weight-bold mb-0">Event:</p>
+                          <h6 class="text-sm mb-0">Rapat Bulanan</h6>
                         </div>
                       </div>
                     </td>
                     <td>
                       <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                        <h6 class="text-sm mb-0">3.900</h6>
+                        <p class="text-xs font-weight-bold mb-0">Time:</p>
+                        <h6 class="text-sm mb-0">17:00</h6>
                       </div>
                     </td>
                     <td>
                       <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Value:</p>
-                        <h6 class="text-sm mb-0">$440,000</h6>
+                        <p class="text-xs font-weight-bold mb-0">Place:</p>
+                        <h6 class="text-sm mb-0">Balaikota</h6>
                       </div>
                     </td>
                     <td class="align-middle text-sm">
                       <div class="col text-center">
-                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                        <h6 class="text-sm mb-0">40.22%</h6>
+                        <p class="text-xs font-weight-bold mb-0">Status:</p>
+                        <h6 class="text-sm mb-0">Internal</h6>
                       </div>
                     </td>
                   </tr>
                   <tr>
                     <td class="w-30">
                       <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
-                          <img src="../assets/img/icons/flags/GB.png" alt="Country flag">
-                        </div>
+                        {{-- <div>
+                          <img src="../assets/img/icons/flags/US.png" alt="Country flag">
+                        </div> --}}
                         <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">Country:</p>
-                          <h6 class="text-sm mb-0">Great Britain</h6>
+                          <p class="text-xs font-weight-bold mb-0">Event:</p>
+                          <h6 class="text-sm mb-0">Rapat Bulanan</h6>
                         </div>
                       </div>
                     </td>
                     <td>
                       <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                        <h6 class="text-sm mb-0">1.400</h6>
+                        <p class="text-xs font-weight-bold mb-0">Time:</p>
+                        <h6 class="text-sm mb-0">17:00</h6>
                       </div>
                     </td>
                     <td>
                       <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Value:</p>
-                        <h6 class="text-sm mb-0">$190,700</h6>
+                        <p class="text-xs font-weight-bold mb-0">Place:</p>
+                        <h6 class="text-sm mb-0">Balaikota</h6>
                       </div>
                     </td>
                     <td class="align-middle text-sm">
                       <div class="col text-center">
-                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                        <h6 class="text-sm mb-0">23.44%</h6>
+                        <p class="text-xs font-weight-bold mb-0">Status:</p>
+                        <h6 class="text-sm mb-0">Internal</h6>
                       </div>
                     </td>
                   </tr>
                   <tr>
                     <td class="w-30">
                       <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
-                          <img src="../assets/img/icons/flags/BR.png" alt="Country flag">
-                        </div>
+                        {{-- <div>
+                          <img src="../assets/img/icons/flags/US.png" alt="Country flag">
+                        </div> --}}
                         <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">Country:</p>
-                          <h6 class="text-sm mb-0">Brasil</h6>
+                          <p class="text-xs font-weight-bold mb-0">Event:</p>
+                          <h6 class="text-sm mb-0">Rapat Bulanan</h6>
                         </div>
                       </div>
                     </td>
                     <td>
                       <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                        <h6 class="text-sm mb-0">562</h6>
+                        <p class="text-xs font-weight-bold mb-0">Time:</p>
+                        <h6 class="text-sm mb-0">17:00</h6>
                       </div>
                     </td>
                     <td>
                       <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Value:</p>
-                        <h6 class="text-sm mb-0">$143,960</h6>
+                        <p class="text-xs font-weight-bold mb-0">Place:</p>
+                        <h6 class="text-sm mb-0">Balaikota</h6>
                       </div>
                     </td>
                     <td class="align-middle text-sm">
                       <div class="col text-center">
-                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                        <h6 class="text-sm mb-0">32.14%</h6>
+                        <p class="text-xs font-weight-bold mb-0">Status:</p>
+                        <h6 class="text-sm mb-0">Internal</h6>
                       </div>
                     </td>
                   </tr>
+                  
                 </tbody>
               </table>
             </div>
@@ -392,8 +393,9 @@
                       <i class="ni ni-mobile-button text-white opacity-10"></i>
                     </div>
                     <div class="d-flex flex-column">
-                      <h6 class="mb-1 text-dark text-sm">Devices</h6>
-                      <span class="text-xs">250 in stock, <span class="font-weight-bold">346+ sold</span></span>
+                      <h6 class="mb-1 text-dark text-sm">Internal</h6>
+                      <span class="text-xs">Acara khusus internal</span>
+                      {{-- <span class="text-xs">250 in stock, <span class="font-weight-bold">346+ sold</span></span> --}}
                     </div>
                   </div>
                   <div class="d-flex">
@@ -406,8 +408,8 @@
                       <i class="ni ni-tag text-white opacity-10"></i>
                     </div>
                     <div class="d-flex flex-column">
-                      <h6 class="mb-1 text-dark text-sm">Tickets</h6>
-                      <span class="text-xs">123 closed, <span class="font-weight-bold">15 open</span></span>
+                      <h6 class="mb-1 text-dark text-sm">Eksternal</h6>
+                      <span class="text-xs">Acara khusus eksternal</span>
                     </div>
                   </div>
                   <div class="d-flex">
@@ -420,8 +422,8 @@
                       <i class="ni ni-box-2 text-white opacity-10"></i>
                     </div>
                     <div class="d-flex flex-column">
-                      <h6 class="mb-1 text-dark text-sm">Error logs</h6>
-                      <span class="text-xs">1 is active, <span class="font-weight-bold">40 closed</span></span>
+                      <h6 class="mb-1 text-dark text-sm">Publik</h6>
+                      <span class="text-xs">Acara untuk publik</span>
                     </div>
                   </div>
                   <div class="d-flex">
@@ -434,8 +436,8 @@
                       <i class="ni ni-satisfied text-white opacity-10"></i>
                     </div>
                     <div class="d-flex flex-column">
-                      <h6 class="mb-1 text-dark text-sm">Happy users</h6>
-                      <span class="text-xs font-weight-bold">+ 430</span>
+                      <h6 class="mb-1 text-dark text-sm">Rahasia</h6>
+                      <span class="text-xs">Acara rahasia</span>
                     </div>
                   </div>
                   <div class="d-flex">
