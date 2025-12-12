2fbe719c01e60287cbe49ab713f7835e7eb9387c
rizal <rizal.palms@gmail.com>
2025-12-12 15:50:02 +0700

    Add search agenda on landing page

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -92,6 +92,39 @@
           <div class="col-lg-12 mb-lg-0 mb-4">
             <div class="card ">
               <div class="card-header pb-0 p-3">
+                {{-- search --}}
+                  <div class="input-group mb-3">
+                    <input type="text" class="form-control" id="searchInput" placeholder="Cari agenda..." onkeyup="searchTable()">
+                  </div>
+                  <script>
+                    function searchTable() {
+                      var input, filter, table, tr, td, i, txtValue;
+                      input = document.getElementById("searchInput");
+                      filter = input.value.toUpperCase();
+                      table = document.querySelector(".table");
+                      tr = table.getElementsByTagName("tr");
+                      for (i = 0; i < tr.length; i++) {
+                        td = tr[i].getElementsByTagName("td");
+                        let rowContainsFilter = false;
+                        for (let j = 0; j < td.length; j++) {
+                          if (td[j]) {
+                            txtValue = td[j].textContent || td[j].innerText;
+                            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                              rowContainsFilter = true;
+                              break;
+                            }
+                          }
+                        }
+                        if (rowContainsFilter) {
+                          tr[i].style.display = "";
+                        } else {
+                          tr[i].style.display = "none";
+                        }
+                      }
+                    }
+                  </script>
+              </div>
+              <div class="card-header pb-0 p-3">
                 <div class="d-flex justify-content-between">
                   <h6 class="mb-2">Hari Ini</h6>
                 </div>
